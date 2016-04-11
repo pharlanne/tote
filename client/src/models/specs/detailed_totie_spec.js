e08903b2ee0a5ffc9576664e5e1b706d1e740e17
A_Lippok <alippok@hotmail.co.uk>
2016-04-11 12:15:16 +0100

    Checks DetailedTotie mmodel returns price level it is present

--- client/src/models/specs/detailed_totie_spec.js
+++ client/src/models/specs/detailed_totie_spec.js
@@ -39,8 +39,49 @@ var detailedPlaceResult = {
   website: "https://www.jdwetherspoon.com/pubs/all-pubs/scotland/edinburgh/the-standing-order-edinburgh",
 }
 
+var detailedPlaceResultTwo = {
+  formatted_address: "20 Princes Street, Edinburgh EH1 1HT, United Kingdom",
+  formatted_phone_number: "0131 4431212",
+  geometry: {
+    location: {
+      lat: 39, 
+      lng: 87
+    }
+  }, 
+  name: "Wetherspoons", 
+  opening_hours: {
+    open_now: true,
+    weekday_text: [
+      "Monday: 11:00 AM - 1:00 AM", 
+      "Tuesday: 11:00 AM - 1:00 AM", 
+      "Wednesday: 11:00 AM - 1:00 AM", 
+      "Thursday: 11:00 AM - 1:00 AM", 
+      "Friday: 11:00 AM - 1:00 AM", 
+      "Saturday: 11:00 AM - 1:00 AM", 
+      "Sunday: 11:00 AM - 1:00 AM", 
+    ]
+  }, 
+  place_id:  "1234", 
+  price_level: 2,
+  rating: 3.5, 
+  reviews: [
+    {
+      rating: 3, 
+      text: "Very good"
+    }, 
+    {
+      rating: 3.5,
+      text: "Decent"
+    }
+  ],
+  types: ["restaurant", "bar", "food"],
+  website: "https://www.jdwetherspoon.com/pubs/all-pubs/scotland/edinburgh/the-standing-order-edinburgh",
+}
+
 
 var detailedTotie1 = new DetailedTotie( detailedPlaceResult.name, detailedPlaceResult.geometry.location.lat, detailedPlaceResult.geometry.location.lng, detailedPlaceResult.formatted_address, detailedPlaceResult.place_id, detailedPlaceResult.opening_hours.open_now, detailedPlaceResult.opening_hours.weekday_text, detailedPlaceResult.price_level )
+
+var detailedTotie2 = new DetailedTotie( detailedPlaceResultTwo.name, detailedPlaceResultTwo.geometry.location.lat, detailedPlaceResultTwo.geometry.location.lng, detailedPlaceResultTwo.formatted_address, detailedPlaceResultTwo.place_id, detailedPlaceResultTwo.opening_hours.open_now, detailedPlaceResultTwo.opening_hours.weekday_text, detailedPlaceResultTwo.price_level )
 
 describe("Detailed Totie", function(){
   it("should be able to store a name", function(){
@@ -81,6 +122,10 @@ describe("Detailed Totie", function(){
 
   it("should be return undefined if price level is not present", function(){
     assert.equal(undefined, detailedTotie1.priceLevel)
+  })
+
+  it("should return price level if it is present", function(){
+    assert.equal(2, detailedTotie2.priceLevel)
   })
 
 
