9e99267bd595b312c5c4e6b543e15d400a875a68
A_Lippok <alippok@hotmail.co.uk>
2016-04-11 10:30:30 +0100

    Checks BasicTotie model can handle a missing price level

--- client/src/models/basic_totie.js
+++ client/src/models/basic_totie.js
@@ -1,13 +1,14 @@
 
 
 
-var BasicTotie = function(name, lat, lng, placeId, rating, types ){
+var BasicTotie = function(name, lat, lng, placeId, priceLevel, rating, types ){
   this.name = name, 
   this.location = {
     lat: lat,
     lng: lng
   },
   this.placeId = placeId,
+  this.priceLevel = priceLevel,
   this.rating = rating, 
   this.types = types
 }
--- client/src/models/specs/basic_totie_spec.js
+++ client/src/models/specs/basic_totie_spec.js
@@ -25,14 +25,15 @@ var basicPlaceResultTwo = {
   }, 
   name: "Wetherspoons", 
   placeId: "1234", 
+  price_level: 2,
   rating: 3, 
   types: ["night_club", "bar"], 
   vicinity: "20 Princes Street, Edinburgh"
 }
 
-var basicTotie = new BasicTotie(basicPlaceResult.name, basicPlaceResult.geometry.location.lat, basicPlaceResult.geometry.location.lng, basicPlaceResult.placeId, basicPlaceResult.rating, basicPlaceResult.types);
+var basicTotie = new BasicTotie(basicPlaceResult.name, basicPlaceResult.geometry.location.lat, basicPlaceResult.geometry.location.lng, basicPlaceResult.placeId, basicPlaceResult.price_level, basicPlaceResult.rating, basicPlaceResult.types);
 
-var basicTotieTwo = new BasicTotie(basicPlaceResult.name, basicPlaceResult.geometry.location.lat, basicPlaceResult.geometry.location.lng, basicPlaceResult.placeId, basicPlaceResult.rating, basicPlaceResultTwo.types);
+var basicTotieTwo = new BasicTotie(basicPlaceResultTwo.name, basicPlaceResultTwo.geometry.location.lat, basicPlaceResultTwo.geometry.location.lng, basicPlaceResultTwo.placeId, basicPlaceResultTwo.price_level, basicPlaceResultTwo.rating, basicPlaceResultTwo.types);
 
 describe("totie", function(){
   it("should store a name", function(){
@@ -56,7 +57,11 @@ describe("totie", function(){
   });
 
   it("should be able to store multiple types", function(){
-    assert.deepEqual(["night_club", "bar"], basicTotieTwo.types)
+    assert.deepEqual(["night_club", "bar"], basicTotieTwo.types);
+  });
+
+  it("should return undefined if price level is not present", function(){
+    assert.equal(undefined, basicTotie.price_level)
   })
 
 
