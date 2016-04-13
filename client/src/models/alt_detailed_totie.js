60c9bb1e3e1fa271605254d165ae41e217fc5d82
A_Lippok <alippok@hotmail.co.uk>
2016-04-13 08:33:06 +0100

    Makes changes to original detailed totie model and begins work on an alternate detailed totie model

---
+++ client/src/models/alt_detailed_totie.js
@@ -0,0 +1,26 @@
+
+
+
+var AltDetailedTotie = function(params){
+  this.name = params["name"]
+  // this.location = {
+  //   lat: params["lat"], 
+  //   lng: params["lng"]
+  // }, 
+  // this.address = params["address"],
+  // this.phoneNumber = params["phoneNumber"],
+  // this.placeId = params["placeId"],
+  // this.priceLevel = params["priceLevel"],
+  // this.rating = params["rating"],
+  // this.reviews = params["reviews"],
+  // this.types = params["types"],
+  // this.website = params["website"]
+
+}
+
+
+
+
+
+
+module.exports = AltDetailedTotie;
---
+++ client/src/models/specs/alt_detailed_totie_spec.js
@@ -0,0 +1,59 @@
+var assert = require('chai').assert;
+var AltDetailedTotie = require('../alt_detailed_totie.js');
+
+
+
+var detailedPlaceResult = {
+  formatted_address: "20 Princes Street, Edinburgh EH1 1HT, United Kingdom",
+  formatted_phone_number: "0131 4431212",
+  geometry: {
+    location: {
+      lat: 39, 
+      lng: 87
+    }
+  }, 
+  name: "Wetherspoons", 
+  opening_hours: {
+    weekday_text: [
+      "Monday: 11:00 AM - 1:00 AM", 
+      "Tuesday: 11:00 AM - 1:00 AM", 
+      "Wednesday: 11:00 AM - 1:00 AM", 
+      "Thursday: 11:00 AM - 1:00 AM", 
+      "Friday: 11:00 AM - 1:00 AM", 
+      "Saturday: 11:00 AM - 1:00 AM", 
+      "Sunday: 11:00 AM - 1:00 AM", 
+    ]
+  }, 
+  place_id:  "1234", 
+  rating: 3.5, 
+  reviews: [
+    {
+      rating: 3, 
+      text: "Very good"
+    }, 
+    {
+      rating: 3.5,
+      text: "Decent"
+    }
+  ],
+  types: ["restaurant", "bar", "food"],
+  website: "https://www.jdwetherspoon.com/pubs/all-pubs/scotland/edinburgh/the-standing-order-edinburgh",
+}
+
+
+var params = {
+  name: detailedPlaceResult.name
+}
+
+var altDetailedTotie1 = new AltDetailedTotie(params)
+
+
+
+describe("alternate detailed totie", function(){
+  it("should take in a name from a params object", function(){
+    assert.equal("Wetherspoons", altDetailedTotie1.name)
+  })
+
+
+
+})
